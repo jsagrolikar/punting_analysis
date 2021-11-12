2dc4938bc48373cca713c6c625058ecf3f6e20d9
Jay Sagrolikar <jsagrolikar@uchicago.edu>
2021-11-12 17:47:23 -0600

    comments, getting rid of tests

--- clustering_scratchwork.R
+++ clustering_scratchwork.R
@@ -2,19 +2,24 @@ library(ggplot2)
 library(dplyr)
 library(gtools)
 library(data.table)
-wd <- "C:/Users/pauli/OneDrive - The University of Chicago/Documents/NFL Research/Big Data Bowl 2022/nfl-big-data-bowl-2022"
+library(cluster)
+library(tidyverse)
+
+wd <- "C:/Users/Jay Sagrolikar/punting_analysis/data"
 setwd(wd)
 # tracking_sample <- read.csv("tracking2020.csv")
 tracking_sample <- read.csv("2020_reduced.csv")
 tracking_sample$ï..time <- NULL
+games_df <- read.csv("games.csv")
+plays_df <- read.csv("plays.csv")
+players_df <- read.csv("players.csv")
+
+## modifying left direction coordinates, binding to right
 left_direction_df <- tracking_sample[tracking_sample$playDirection=="left",]
 left_direction_df$y <- 53.3-left_direction_df$y
 left_direction_df$x <- 120-left_direction_df$x
 tracking_sample <- tracking_sample[tracking_sample$playDirection!="left",]
 tracking_sample <- rbind(tracking_sample, left_direction_df)
-games_df <- read.csv("games.csv")
-plays_df <- read.csv("plays.csv")
-players_df <- read.csv("players.csv")
 
 # blocked_punts <- tracking_sample[tracking_sample$event=="punt_blocked",]
 # blocked_punt_summary <- unique(data.frame(gameId=blocked_punts$gameId, playId=blocked_punts$playId, frameId=blocked_punts$frameId))
@@ -23,11 +28,13 @@ players_df <- read.csv("players.csv")
 # blocked_punts_df$time <- NULL
 # exp_df <- rbind(tracking_sample, blocked_punts_df)
 
-
+## filtering for punts
 poss_team_df <- data.frame(possessionTeam=plays_df$possessionTeam, gameId=plays_df$gameId, playId=plays_df$playId)
 punt_instance_df <- tracking_sample[tracking_sample$event=="punt",]
 punt_info_index <- unique(data.frame(gameId=punt_instance_df$gameId, playId=punt_instance_df$playId, punt_event_frame=punt_instance_df$frameId))
 punt_events_df <- merge(tracking_sample, punt_info_index, by=c("gameId", "playId"))
+
+## gets football location data and makes a df with LOS information
 ball_snap_punt_df <- punt_events_df[punt_events_df$event=="ball_snap",]
 ball_snap_punt_info_index <- unique(data.frame(gameId=ball_snap_punt_df$gameId, playId=ball_snap_punt_df$playId, ball_snap_event_frame=ball_snap_punt_df$frameId))
 punt_info_index <- merge(punt_info_index, ball_snap_punt_info_index, by=c("gameId","playId"))
@@ -36,7 +43,10 @@ punt_events_ball_at_snap_df <- punt_events_df[punt_events_df$displayName=="footb
 los_info_df <- data.frame(gameId=punt_events_ball_at_snap_df$gameId,playId=punt_events_ball_at_snap_df$playId,los_x=punt_events_ball_at_snap_df$x, los_y=punt_events_ball_at_snap_df$y)
 punt_events_df$frames_after_snap <- punt_events_df$frameId-punt_events_df$ball_snap_event_frame
 
+## no football rows
 punt_events_df <- punt_events_df[!punt_events_df$displayName=="football", ]
+
+## combines original punt data with LOS data
 # punt_events_df$rounded_y <- round(punt_events_df$y)
 punt_events_df <- merge(punt_events_df, games_df, by=c("gameId"))
 punt_events_df <- merge(punt_events_df, poss_team_df, by=c("gameId", "playId"))
@@ -45,34 +55,11 @@ punt_events_df$TeamAbbr[punt_events_df$team=="home"] <- punt_events_df$homeTeamA
 punt_events_df$TeamAbbr[punt_events_df$team=="away"] <- punt_events_df$visitorTeamAbbr[punt_events_df$team=="away"]
 punt_events_df <- merge(punt_events_df, los_info_df, by=c("gameId","playId"))
 punt_events_df$x <- punt_events_df$x-punt_events_df$los_x
-punt_events_df$y <- punt_events_df$y-punt_events_df$los_y ###unsure if we should normalize y to LOS; likely insignificant but should discuss
-
-punt1_df <- punt_events_df[punt_events_df$playId==punt_info_index$playId[1] & punt_events_df$gameId==punt_info_index$gameId[1],]
-punt1_df <- punt1_df[punt1_df$frameId>=punt1_df$ball_snap_event_frame & punt1_df$frameId<=punt1_df$punt_event_frame,]
-# punt1_df$frames_after_snap <- punt1_df$frameId-punt1_df$ball_snap_event_frame
-punt1_frame0_df <- punt1_df[punt1_df$frames_after_snap==0,]
-punt1_frame0_df<- punt1_frame0_df[order(punt1_frame0_df$y, punt1_frame0_df$x),]
-punt1_frame0_punting_df <- punt1_frame0_df[punt1_frame0_df$possessionTeam==punt1_frame0_df$TeamAbbr,]
-punt1_frame0_punting_df <- rbind(punt1_frame0_punting_df[punt1_frame0_punting_df$position=="P",],punt1_frame0_punting_df[punt1_frame0_punting_df$position!="P",])
-punt1_frame0_receiving_df <- punt1_frame0_df[punt1_frame0_df$possessionTeam!=punt1_frame0_df$TeamAbbr,]
-punt1_frame0_receiving_df <- rbind(punt1_frame0_receiving_df[punt1_frame0_receiving_df$x==max(punt1_frame0_receiving_df$x),],punt1_frame0_receiving_df[punt1_frame0_receiving_df$x!=max(punt1_frame0_receiving_df$x),])
-###should we omit receiving player from clustering?
+punt_events_df$y <- punt_events_df$y-punt_events_df$los_y 
+## unsure if we should normalize y to LOS; likely insignificant but should discuss
 
 
-punt2_df <- punt_events_df[punt_events_df$playId==punt_info_index$playId[2] & punt_events_df$gameId==punt_info_index$gameId[2],]
-punt2_df <- punt2_df[punt2_df$frameId>=punt2_df$ball_snap_event_frame & punt2_df$frameId<=punt2_df$punt_event_frame,]
-# punt2_df$frames_after_snap <- punt2_df$frameId-punt2_df$ball_snap_event_frame
-punt2_frame0_df <- punt2_df[punt2_df$frames_after_snap==0,]
-punt2_frame0_df<- punt2_frame0_df[order(punt2_frame0_df$y, punt2_frame0_df$x),]
-punt2_frame0_punting_df <- punt2_frame0_df[punt2_frame0_df$possessionTeam==punt2_frame0_df$TeamAbbr,]
-punt2_frame0_punting_df <- rbind(punt2_frame0_punting_df[punt2_frame0_punting_df$position=="P",],punt2_frame0_punting_df[punt2_frame0_punting_df$position!="P",])
-punt2_frame0_receiving_df <- punt2_frame0_df[punt2_frame0_df$possessionTeam!=punt2_frame0_df$TeamAbbr,]
-punt2_frame0_receiving_df <- rbind(punt2_frame0_receiving_df[punt2_frame0_receiving_df$x==max(punt2_frame0_receiving_df$x),],punt2_frame0_receiving_df[punt2_frame0_receiving_df$x!=max(punt2_frame0_receiving_df$x),])
-#could try with matching algorithm that moves a player at a lower depth than others around to see optimality
-
-total_cluster_distance <- sum(sqrt(((punt1_frame0_punting_df$x-punt2_frame0_punting_df$x)^2)+((punt1_frame0_punting_df$y-punt2_frame0_punting_df$y)^2)))+
-  sum(sqrt(((punt1_frame0_receiving_df$x-punt2_frame0_receiving_df$x)^2)+((punt1_frame0_receiving_df$y-punt2_frame0_receiving_df$y)^2)))
-
+## filtering for frames within snap and punt
 filt_punt_events_df <- punt_events_df[punt_events_df$frames_after_snap>=0 & punt_events_df$frames_after_snap<=(punt_events_df$punt_event_frame-punt_events_df$ball_snap_event_frame),]
 
 for (frame_id in c(0:max(filt_punt_events_df$frames_after_snap))[1]) {
@@ -82,8 +69,8 @@ for (frame_id in c(0:max(filt_punt_events_df$frames_after_snap))[1]) {
   punt_index_df <- (unique(data.frame(gameId=frame_punt_events_df$gameId, playId=frame_punt_events_df$playId)))
   punt_index_df$punt_id <- 1:nrow(punt_index_df)
   row.names(punt_index_df) <- NULL
-  n <- (nrow(punt_index_df)-1)
   
+  n <- (nrow(punt_index_df)-1)
   dist_mat <- matrix(nrow=n, ncol=n)
   formation_list <- list()
   for (i in c(1:n)) {
@@ -120,14 +107,4 @@ for (frame_id in c(0:max(filt_punt_events_df$frames_after_snap))[1]) {
     # print(total_cluster_distance)
 
   }
-  formation_df_all <- rbindlist(formation_list)
-  
-  baseline_score <- mean(formation_df_all$formation_distance)
-  # combinations(length(unique(formation_df_all$gameId_1)), 2, unique(formation_df_all$gameId_1))
 }
-
-frame0_comp <- rbind(punt1_df[punt1_df$frames_after_snap==0,], punt2_df[punt2_df$frames_after_snap==0,])
-
-
-length(unique(punt1_df$nflId))
-
