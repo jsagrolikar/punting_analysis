b4c60574792841bccdaeaeaa6790de3956db8c01
jsagrolikar <87156932+jsagrolikar@users.noreply.github.com>
2021-12-24 12:27:48 -0500

    Update updated_clustering.R

--- updated_clustering.R
+++ updated_clustering.R
@@ -31,6 +31,25 @@ make_dist_mat <- function(p, plist) {
   return(dist_mat)
 }
 
+make_dist_mat2 <- function(p, idlist, plist) {
+  ## calculates distance matrix for punts left in the list 
+  dist_mat <- matrix(nrow=p, ncol=p)
+  for (i in 1:(p-1)) {
+    id1 <- idlist[[i]]
+    for (j in 1:(p-1)) {
+      id2 <- idlist[[j+1]]
+      dist_mat[i, j+1] <- punt_dist(id1, id2, plist)
+    }
+  }
+  ## removes zeroes
+  dist_mat[lower.tri(dist_mat, diag=TRUE)] <- Inf
+  return(dist_mat)
+}
+
+na.omit.list <- function(y) { 
+  return(y[!sapply(y, function(x) all(is.na(x)))]) 
+}
+
 ## sets sample size
 n <- (nrow(frame_punt_events_df)/22)-1
 
@@ -58,10 +77,29 @@ new_n <- n
 iteration_scores <- as.data.frame(matrix(0, ncol = 2, nrow = n))
 colnames(iteration_scores) <- c("number of clusters", "average distance within clusters")
 iteration_scores[new_n, 1] <- new_n
+information_list <- list()
+preserved_cluster <- NA
 
 while (n>=2) {
   
-  dist_mat <- make_dist_mat(n, punts_lists)
+  if (is.na(preserved_cluster)==FALSE) {
+    
+    dist_mat <- dist_mat[-unpreserved_cluster,-unpreserved_cluster]
+    dist_vec1 <- c(rep(Inf,(n)))
+    dist_vec2 <- c(rep(Inf,(n)))
+    for (i in 1:preserved_cluster) {
+      dist_vec1[i] <- punt_dist(preserved_cluster, i, punts_lists)
+    }
+    for (i in preserved_cluster:n) {
+      dist_vec2[i] <- punt_dist(preserved_cluster, i, punts_lists)
+    }
+    dist_mat[,preserved_cluster] <- dist_vec1
+    dist_mat[preserved_cluster,] <- dist_vec2
+    dist_mat[lower.tri(dist_mat, diag=TRUE)] <- Inf
+    
+  } else {
+    dist_mat <- make_dist_mat(n, punts_lists)
+  }
   
   loc_mat <- as.data.frame(which(dist_mat==min(dist_mat), arr.ind=TRUE))
   minrow <- as.numeric(loc_mat$row[1])
@@ -103,7 +141,6 @@ while (n>=2) {
   ids_in_cluster <- c(as.vector(na.omit(info_df[,preserved_cluster_id])),as.vector(na.omit(info_df[,unpreserved_cluster_id])))
   info_df[,preserved_cluster_id] <- c(ids_in_cluster,rep(NA,new_n-length(ids_in_cluster)))
   info_df[,unpreserved_cluster_id] <- NA
-  print(info_df)
   
   if (n == new_n) {
     iteration_scores[1, 1] <- 1
@@ -111,34 +148,53 @@ while (n>=2) {
   }
   
   cluster_df <- info_df[,colSums(is.na(info_df))<nrow(info_df)]
+  information_list[[n-1]] <- as.data.frame(cluster_df)
+  # information_list <- append(information_list, as.data.frame(cluster_df))
+  total_cluster_distances <- 0
   if (!is.null(ncol(cluster_df))) {
     for (i in 1:ncol(cluster_df)) {
       punt_ids <- list()
       distancescore <- 0
       for (j in 1:nrow(cluster_df)) {
-        punt_ids <- append(punt_ids, j)
+        punt_ids <- append(punt_ids, cluster_df[j, i])
       }
-      punt_ids <- na.omit(punt_ids)
-      cluster_mat <- make_dist_mat(length(punt_ids), all_punts)
-      cluster_dist <- mean(cluster_mat[is.finite(cluster_mat)])
+      punt_ids <- na.omit.list(punt_ids)
+      if (length(punt_ids) <= 1) {
+        cluster_dist <- 0
+      } else {
+        cluster_mat <- make_dist_mat2(length(punt_ids), punt_ids, all_punts)
+        cluster_dist <- mean(cluster_mat[is.finite(cluster_mat)])
+      }
       distancescore <- distancescore + cluster_dist
+      total_cluster_distances <- total_cluster_distances + distancescore
       
     }
-    
-    distancescore <- distancescore/ncol(cluster_df)
+
+    total_cluster_distances <- total_cluster_distances/ncol(cluster_df)
     
     iteration_scores[n-1, 1] <- ncol(cluster_df)
-    iteration_scores[n-1, 2] <- distancescore
+    iteration_scores[n-1, 2] <- total_cluster_distances
+    iteration_scores$total_distance <- iteration_scores$`number of clusters`*iteration_scores$`average distance within clusters`
+    
   }
   
   n <- n-1
   
 }
-  
 
-  
+iteration_scores$iteration_scores <- (iteration_scores$`number of clusters`/max(iteration_scores$`number of clusters`))+
+  (iteration_scores$`average distance within clusters`/max(iteration_scores$`average distance within clusters`))
+mincluster <- iteration_scores[iteration_scores$iteration_scores==min(iteration_scores$iteration_scores),]$`number of clusters`
+finalclusters <- information_list[[mincluster]]
+result <- finalclusters
 
-  
-  
-  
-  
+for (i in 1:ncol(result)) {
+  for (j in 1:nrow(result)) {
+    if (is.na(result[j, i])) {
+      result[j, i] <- "0"
+    } else {
+      result[j, i] <- paste(as.character(all_punts[[3*as.numeric(result[j,i])]]$gameId[1]), "/", as.character(all_punts[[3*as.numeric(result[j,i])]]$playId[1]), sep="")
+    }
+  }
+}
+
